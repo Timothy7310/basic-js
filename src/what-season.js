967563ee77169e3c6392ffb884c306f486376fd6
Timothy7310 <penlist127@gmail.com>
2022-04-20 20:03:11 +0300

    feat: add what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,7 +13,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason( /* date */ ) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
@@ -19,3 +21,27 @@ function getSeason(/* date */) {
 module.exports = {
   getSeason
 };
+
+
+function getSeason(date) {
+  if (date === null || date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  try {
+    date.getTime();
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
+
+  let numOfMounth = date.getMonth();
+  if (numOfMounth === 11 || numOfMounth === 0 || numOfMounth === 1) {
+    return 'winter';
+  } else if (numOfMounth === 2 || numOfMounth === 3 || numOfMounth === 4) {
+    return 'spring';
+  } else if (numOfMounth === 5 || numOfMounth === 6 || numOfMounth === 7) {
+    return 'summer';
+  } else if (numOfMounth === 8 || numOfMounth === 9 || numOfMounth === 10) {
+    return 'autumn';
+  }
+
+}
