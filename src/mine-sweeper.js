1aeb04330a08c23f8be00b1209844bf12e95ee25
Timothy7310 <penlist127@gmail.com>
2022-04-26 13:21:12 +0300

    feat: add mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let count = 0;
+  const copyArr = JSON.parse(JSON.stringify(matrix))
+  console.log(copyArr);
+  for(let i = 0; i < matrix.length; i++){
+    for(let k = 0; k < matrix[i].length; k++){
+
+      if(matrix[i][k + 1] === true){
+        count++;
+      }
+      if(matrix[i][k - 1] === true){
+        count++;
+      }
+
+      if(matrix[i + 1] !== undefined){
+        if(matrix[i + 1][k] === true){
+          count++;
+        }
+        if(matrix[i + 1][k - 1] === true){
+          count++;
+        }
+        if(matrix[i + 1][k + 1] === true){
+          count++;
+        }
+      }
+
+      if (matrix[i - 1] !== undefined) {
+        if(matrix[i - 1][k] === true){
+          count++;
+        }
+        if(matrix[i - 1][k - 1] === true){
+          count++;
+        }
+        if(matrix[i - 1][k + 1] === true){
+          count++;
+        }
+      }
+      
+      copyArr[i][k] = count;
+      count = 0;
+    }
+  }
+  console.log(matrix);
+  console.log(copyArr);
+  return copyArr;
 }
 
 module.exports = {
   minesweeper
 };
+
+
+let matrix = [
+  [true, false, false],
+  [false, true, false],
+  [false, false, false]
+]
+
+  minesweeper(matrix);
